a7f0c16105fd6b6224088a6b392f67bfea758212
algapry <alihameed7121996ph@gmail.com>
2024-03-15 08:43:35 +0300

    Uploade

---
+++ main.js
@@ -0,0 +1,66 @@
+let links_icon = document.getElementById("links_icon");
+let ul = document.getElementById("ul_links");
+
+
+links_icon.onclick = function () {
+    ul_links.style.setProperty("display", "block");
+   };
+links_icon.ondblclick = function () {
+    ul_links.style.setProperty("display", "none");
+};
+   
+let show_1 = document.getElementById("show1");
+let caption_1 = document.getElementById("cap1");
+let hide_icon = document.getElementById("hide_icon1");
+show_1.onclick = function () {
+    caption_1.style.setProperty("display", "block");
+};
+hide_icon.onclick = function () {
+    caption_1.style.setProperty("display", "none");
+};
+hide_icon.onmouseleave = function () {
+    caption_1.style.setProperty("display", "none");
+};
+
+
+let show_2 = document.getElementById("show2");
+let caption_2 = document.getElementById("cap2");
+show_2.onclick = function () {
+    caption_2.style.setProperty("display", "block");
+};
+
+let show_3 = document.getElementById("show3");
+let caption_3 = document.getElementById("cap3");
+show_3.onclick = function () {
+    caption_3.style.setProperty("display", "block");
+};
+
+let show_4 = document.getElementById("show4");
+let caption_4 = document.getElementById("cap4");
+show_4.onclick = function () {
+    caption_4.style.setProperty("display", "block");
+};
+
+let show_5 = document.getElementById("show5");
+let caption_5 = document.getElementById("cap5");
+show_5.onclick = function () {
+    caption_5.style.setProperty("display", "block");
+};
+
+let show_6 = document.getElementById("show6");
+let caption_6 = document.getElementById("cap6");
+show_6.onclick = function () {
+    caption_6.style.setProperty("display", "block");
+};
+
+let show_7 = document.getElementById("show7");
+let caption_7 = document.getElementById("cap7");
+show_7.onclick = function () {
+    caption_7.style.setProperty("display", "block");
+};
+
+let show_8 = document.getElementById("show8");
+let caption_8 = document.getElementById("cap8");
+show_8.onclick = function () {
+    caption_8.style.setProperty("display", "block");
+};
